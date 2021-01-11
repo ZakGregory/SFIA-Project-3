26af5c0f3e72c09f0134304e6e27641e12eb2457
George-Rhodes <george_rhodes@hotmail.co.uk>
2021-01-11 16:01:24 +0000

    hopeful change to script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sh  ./finalQA/terraform/environments/production/production_env_setup.sh
+sudo sh  ./finalQA/terraform/environments/production/production_env_setup.sh
 
 #source f/terraform/environments/testing/testing_env_setup,sh
