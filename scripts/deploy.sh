975233c7abd9c146fb73266201e2a028f5b3dd0d
George-Rhodes <george_rhodes@hotmail.co.uk>
2021-01-12 11:43:43 +0000

    changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd K8s/
-kubectl create -f ./K8s/backend/backend.yaml
-kubectl create -f ./K8s/database/database.yaml
-kubectl create -f ./K8s/database/databasepersistent.yaml
-kubectl create -f ./K8s/front-end/front-end.yaml
-kubectl create -f ./K8s/nginx/nginx.yaml
+kubectl create -f ./backend/backend.yaml
+kubectl create -f ./database/database.yaml
+kubectl create -f ./database/databasepersistent.yaml
+kubectl create -f ./front-end/front-end.yaml
+kubectl create -f ./nginx/nginx.yaml
