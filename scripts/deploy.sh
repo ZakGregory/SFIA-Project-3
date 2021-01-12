0c1708abdc3d8a84e4adf1b29464629ab00a6dae
Ubuntu <team4@jenkins.ipat2eafby2ejiu1lbydbj5iea.zx.internal.cloudapp.net>
2021-01-12 11:29:58 +0000

    updated

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-kubectl apply -f ./K8s/backend/backend.yaml
-kubectl apply -f ./K8s/database/database.yaml
-kubectl apply -f ./K8s/database/databasepersistent.yaml
-kubectl apply -f ./K8s/front-end/front-end.yaml
-kubectl apply -f ./K8s/nginx/nginx.yaml
+kubectl create -f ./K8s/backend/backend.yaml
+kubectl create -f ./K8s/database/database.yaml
+kubectl create -f ./K8s/database/databasepersistent.yaml
+kubectl create -f ./K8s/front-end/front-end.yaml
+kubectl create -f ./K8s/nginx/nginx.yaml
