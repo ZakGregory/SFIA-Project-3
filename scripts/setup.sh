1821d93696ddb5fd8f0bfb27e8ef72a5669d32ef
George-Rhodes <george_rhodes@hotmail.co.uk>
2021-01-12 11:04:59 +0000

    changed script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -17,5 +17,4 @@ sudo terraform apply "production_plan"
 echo "Connect to AKS cluster----------------"
 
 az aks get-credentials --resource-group productionResource --name AKSCluster
-
-cd ./petclinic
+ls
