1ebb557f9538c8e681478b13547e7ee452d877a8
George-Rhodes <george_rhodes@hotmail.co.uk>
2021-01-14 21:00:25 +0000

    add all pods/possible versioning

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker system prune -a -f
-export BUILD_NUMBER="1"
+export BUILD_NUMBER="2"
 docker login 
 docker-compose build
 
--- scripts/push.sh
+++ scripts/push.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
-docker login 
 docker-compose push
 docker system prune -a -f
