42ffe120e3bffa76abe54aacda6f56f7f11c7de6
George-Rhodes <george_rhodes@hotmail.co.uk>
2021-01-14 16:15:47 +0000

    possible change

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@ cd K8s/
 sudo kubectl apply -f ./backend/backend.yaml
 sudo kubectl apply -f ./database/database.yaml
 sudo kubectl apply -f ./database/databasepersistent.yaml
-sudo kubectl apply -f ./front-end/front-end.yaml
+sudo kubectl apply -f ./front-end/front-end.yaml --force=true
 sudo kubectl apply -f ./nginx/nginx.yaml
